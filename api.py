44fc1af689b0a6336b01cad21fa587488abcbdcf
Miguel Madrid Mencía <miguel.madrid@bsc.es>
2018-05-11 12:55:21 +0200

    Add participant status endpoint

--- api.py
+++ api.py
@@ -2,6 +2,7 @@
 import json
 import logging
 import argparse
+import sys
 
 from flask import Flask, session, redirect, url_for, escape, request, jsonify
 
@@ -34,7 +35,6 @@ def index():
         responses:
             200:
                 description: returned index string
-
     """
     return 'Index'
 
@@ -48,15 +48,31 @@ def submissions():
         responses:
             200:
                 description: It returns the submissions associated to the participants identified by the login credentials (the same credential could manage more than one participant), as a JSON array of URIs in the form /submission/{benchmarking event id}/{participant id}/status.
-
-
     """
     return 'TODO: submissions'
+
+
+@app.route('/submission/<int:benchmarking_event_id>/<int:participant_id>/status ')
+def participant_status():
+    """Participant status endpoint.
+    ---
+    get:
+        description: Participant status endpoint description
+        responses:
+            200:
+                description: "JSON containing the state: open, checking, checked, wrong, queued, evaluating, valid, signed-off. The JSON also contains a brief report with the potential links to be used next."
+            404:
+                description: It can also return 404 on error (the benchmarking event id does not exist, or the participant is not enrolled in this event)
+            400:
+                description: generic error
+    """
+    return 'TODO: participant_status'
 
 
 with app.test_request_context():
     spec.add_path(view=index)
     spec.add_path(view=submissions)
+    spec.add_path(view=participant_status)
 
 if __name__ == '__main__':
 
@@ -71,7 +87,7 @@ if __name__ == '__main__':
             with open(f'public/spec/{spec_filename}', 'w') as api_file:
                 json.dump(spec.to_dict(), api_file, indent=2, sort_keys=True)
             print(
-                f'\033[32m{spec_filename} spec generated and validated against OpenAPIv3 succesfully!\033[0m')
+                f'\033[32m{spec_filename} spec generated and validated against OpenAPIv3 succesfully!\033[0m', file=sys.stderr)
         except:
             print(
                 f"\033[31mError: {spec_filename} couldn't be validated against OpenAPIv3!\033[0m", file=sys.stderr)
